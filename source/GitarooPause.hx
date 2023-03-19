52696a0f3a48ac4f8dd106216666191473a1853f
Mini Raposa Gamer <115371844+MiniRaposaGAMER@users.noreply.github.com>
2023-03-19 13:10:25 -0300

    Update GitarooPause.hx

--- source/GitarooPause.hx
+++ source/GitarooPause.hx
@@ -59,15 +59,11 @@ class GitarooPause extends MusicBeatState
 		if (controls.ACCEPT)
 		{
 			if (FlxG.random.int = 1)
-			{
 			PlayState.SONG = Song.loadFromJson('flying-hard', 'flying');
                         LoadingState.loadAndSwitchState(new PlayState());
-			}
 			else if (FlxG.random.int = 2)
-			{
 			PlayState.SONG = Song.loadFromJson('lights-deactivated-hard', 'lights-deactivated');
                         LoadingState.loadAndSwitchState(new PlayState());
-			}
 		}
 
 		super.update(elapsed);
